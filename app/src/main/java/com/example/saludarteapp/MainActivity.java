d5eb0d8fed96b095aedfd00a1b4292d277e528a3
ticaso <ticaso999@gmail.com>
2023-09-02 11:12:02 -0500

    base de datos en tiempo real de sonidos

--- app/src/main/java/com/example/saludarteapp/MainActivity.java
+++ app/src/main/java/com/example/saludarteapp/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.saludarteapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Sound> soundList;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicializar la referencia de la base de datos de Firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Inicializar el ArrayList
+        soundList = new ArrayList<>();
+
+        // Enlazar el ListView con el ArrayList usando un ArrayAdapter
+        final ListView soundListView = findViewById(R.id.lv_soundList);
+        final ArrayAdapter<Sound> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, soundList);
+        soundListView.setAdapter(arrayAdapter);
+
+        // Leer los sonidos desde Firebase
+        // Leer los sonidos desde Firebase
+        mDatabase.child("sounds").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                soundList.clear();  // Vaciar la lista antes de agregar nuevos elementos
+                for (DataSnapshot soundSnapshot : dataSnapshot.getChildren()) {
+                    Sound sound = soundSnapshot.getValue(Sound.class);
+                    soundList.add(sound);
+                }
+                arrayAdapter.notifyDataSetChanged();  // Notificar al adaptador que los datos han cambiado
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Código para manejar errores
+            }
+        });
+
+
+        // Agregar un OnClickListener al ListView
+        // Agregar un OnClickListener al ListView
+        soundListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtener el sonido clickeado
+                Sound clickedSound = soundList.get(position);
+
+                if (clickedSound.getName() != null) {
+                    // Cambiar el estado del sonido en Firebase Realtime Database
+                    mDatabase.child("sounds").child(clickedSound.getName()).child("status").setValue(true);
+
+                    // Mostrar un Toast
+                    Toast.makeText(MainActivity.this, "Has clickeado en: " + clickedSound.getName(), Toast.LENGTH_SHORT).show();
+                } else {
+                    // Mostrar un mensaje de error
+                    Toast.makeText(MainActivity.this, "El sonido clickeado tiene un nombre nulo.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/saludarteapp/Sound.java
+++ app/src/main/java/com/example/saludarteapp/Sound.java
@@ -1,17 +1,32 @@
+package com.example.saludarteapp;
+
 public class Sound {
     private String name;
+    private boolean status;
 
-    public Sound(String name) {
+    // Constructor vacío requerido para Firebase
+    public Sound() {
+    }
+
+    public Sound(String name, boolean status) {
         this.name = name;
+        this.status = status;
     }
 
     // Getters y setters
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
