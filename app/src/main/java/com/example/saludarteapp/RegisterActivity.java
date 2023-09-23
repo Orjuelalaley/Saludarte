c40b272402387ff9185b1e0c18ebd202d54b2698
Sebastián Orjuela <sebasorjuela@gmail.com>
2023-09-23 11:01:33 -0500

    Complete the RegisterActivity.java and create User.java

--- app/src/main/java/com/example/saludarteapp/RegisterActivity.java
+++ app/src/main/java/com/example/saludarteapp/RegisterActivity.java
@@ -11,11 +11,16 @@ import android.widget.EditText;
 
 import com.example.saludarteapp.databinding.ActivityRegisterBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +47,18 @@ public class RegisterActivity extends AppCompatActivity {
         progressDialog.setMessage("Registrando Usuario...");
         progressDialog.show();
         binding.btnCompleteRegistration.setVisibility(View.GONE);
+        User user = new User(binding.etRegisterEmail.getText().toString(), binding.etRegisterPassword.getText().toString());
 
+        mAuth.createUserWithEmailAndPassword(binding.etRegisterEmail.getText().toString(),
+                binding.etRegisterPassword.getText().toString()).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                progressDialog.cancel();
+                binding.btnCompleteRegistration.setVisibility(View.VISIBLE);
+                binding.etRegisterEmail.setText("");
+                binding.etRegisterPassword.setText("");
+                DatabaseReference userRef = db.getReference("Users").child(Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
+                userRef.setValue(user);
     }
-
+        });
+    }
 }
