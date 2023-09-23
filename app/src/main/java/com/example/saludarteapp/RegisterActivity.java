c7ea9633d33d83dc3757bccb29fca5499463d8a8
Sebastián Orjuela <sebasorjuela@gmail.com>
2023-09-23 10:28:30 -0500

    More changes

--- app/src/main/java/com/example/saludarteapp/RegisterActivity.java
+++ app/src/main/java/com/example/saludarteapp/RegisterActivity.java
@@ -1,30 +1,21 @@
 package com.example.saludarteapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.example.saludarteapp.databinding.ActivityRegisterBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
     private FirebaseAuth mAuth;
-    private EditText emailEditText, passwordEditText;
-    private Button registerButton;
-    private ProgressBar progressBar;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +23,25 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        progressBar = new ProgressBar(this);
+        progressDialog = new ProgressDialog(this);
         mAuth = FirebaseAuth.getInstance();
         binding.btnCompleteRegistration.setOnClickListener(v -> registerUser());
-
     }
 
     private void registerUser() {
+
+        if (binding.etRegisterEmail.getText().toString().isEmpty() ||
+                !Patterns.EMAIL_ADDRESS.matcher(binding.etRegisterEmail.getText().toString()).matches()){
+            binding.etRegisterEmail.setError(getString(R.string.mail_error_label));
+            return;
+        } else binding.etRegisterEmail.setError(null);
+        if (binding.etRegisterPassword.getText().toString().isEmpty()){
+            binding.etRegisterPassword.setError(getString(R.string.error_pass_label));
+            return;
+        } else binding.etRegisterPassword.setError(null);
+        progressDialog.setMessage("Registrando Usuario...");
+        progressDialog.show();
+        binding.btnCompleteRegistration.setVisibility(View.GONE);
 
     }
 
